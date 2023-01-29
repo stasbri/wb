0fd4d7ac80db830f699ef91aeecde93daa91dd56
stasbri <stasbritanishskiy@gmail.com>
2023-01-29 13:11:12 +0100

    minor changes

--- run.sh
+++ run.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-python3 main.py
+args=$1
+
+if [ $args == "start" ]
+then
+  echo started
+  python3 get_adds.py &
+fi
+if [ $args == "stop" ]
+then
+  arg=$(ps -aux | grep "python3 get_adds.py")
+  kill $(echo $arg | cut -d" " -f2)
+  echo stopped 
+fi
+
+
