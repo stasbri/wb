00b758eb8c68f9ff1334421450a25936d46e0a15
stasbri <stasbritanishskiy@gmail.com>
2023-01-29 13:49:14 +0100

    updated bash script

--- run.sh
+++ run.sh
@@ -10,7 +10,15 @@ if [ $args == "stop" ]
 then
   arg=$(ps -aux | grep "python3 get_adds.py")
   kill $(echo $arg | cut -d" " -f2)
-  echo stopped 
+  echo stopped
 fi
 
-
+if [ $args == "check" ]
+then
+	if [ $(ps -aux | grep "python3 get_adds.py" | wc -l) == 2 ]
+	then 
+		echo everything is working properly
+	else
+		python3 get_adds.py &
+	fi
+fi
